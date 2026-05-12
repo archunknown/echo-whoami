8ef844dc5e95bd40e09d3a0c57ef5e5e9ed7bd6d
archunknown <adriantasayco99@gmail.com>
2026-05-11 21:30:37 -0500

    fix: Resolucion de dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['react', 'react-dom/client']
+    }
   },
-
   integrations: [react()]
 });
